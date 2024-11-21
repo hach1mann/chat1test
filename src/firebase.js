9aa695790f970c83e26deca5cab1e1bd9aab0fba
Pedro Lisboa <97626835+hach1mann@users.noreply.github.com>
2024-11-21 14:41:43 +0000

    Add files via upload
    the only thing left is node modules :)

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// @author: Pedro Lisboa 2024
+// Protected under MIT License
+const firebaseConfig = {
+  apiKey: "AIzaSyBVKXtWXuGJBwxoOJQGGNJKqVBjKGOgpZY",
+  authDomain: "dark-chat-app.firebaseapp.com",
+  projectId: "dark-chat-app",
+  storageBucket: "dark-chat-app.appspot.com",
+  messagingSenderId: "1040916525785",
+  appId: "1:1040916525785:web:0d0c0e0b0b0b0b0b0b0b"
+};
+
+// PedroLisboa2024: Firebase initialization
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+export { firebaseConfig, db };
